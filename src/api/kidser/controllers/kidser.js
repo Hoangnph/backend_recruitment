45aad2d2dd65b4f18056b03352f201ccafb5e25d
Toan Nguyen <toannk@gmail.com>
2022-12-30 22:41:16 +0700

    Add kidser and partner collection type

---
+++ src/api/kidser/controllers/kidser.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * kidser controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::kidser.kidser');
---
+++ src/api/kidser/routes/kidser.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * kidser router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::kidser.kidser');
