32147c4e336a5a9c6ca8b64ffdc5a6fa7d7b9620
Toan Nguyen <toannk@gmail.com>
2023-01-06 09:05:21 +0700

    Update kidser region

--- src/api/kidser/controllers/kidser.js
+++ src/api/kidser/controllers/kidser.js
@@ -6,4 +6,22 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::kidser.kidser');
+module.exports = createCoreController('api::kidser.kidser', ({ strapi }) => ({
+  async updateKidserRegionAction(ctx) {
+    try {
+      const result = strapi.db.query('api::kidser.kidser').updateMany({
+        where: {
+          region: {
+            $null: true,
+          },
+        },
+        data: {
+          region: 1,
+        },
+      });
+      ctx.body = result;
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+}));
